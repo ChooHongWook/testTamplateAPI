99e0692f198b693b915640b3a91d7527b853e5d9
olt13mm <olt13mm@gmail.com>
2022-09-06 18:09:37 +0900

    refactor: app.ts -> src/server.ts, tsconfig.json 주석 정리

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
+const server = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req: Request, res: Response) => {
+server.get('/', (req: Request, res: Response) => {
   // console.log('접속확인');
   res.send('hello world');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`port:${PORT} server is on!`);
 });
